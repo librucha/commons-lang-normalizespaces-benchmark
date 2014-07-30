eae4f71d33aa0844eb249c4f8b8d40885d72ace6
Libor Ondrusek <libor.ondrusek@artin.cz>
2014-07-30 12:15:20 +0200

    Initial commit

---
+++ src/main/java/org/librucha/benchmark/NormalizeSpaces.java
@@ -0,0 +1,46 @@
+package org.librucha.benchmark;
+
+import com.google.caliper.*;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class NormalizeSpaces extends SimpleBenchmark {
+
+	@Param({ "0", "10", "100", "1000" })
+	private int length;
+
+	public static final String NBSP = String.valueOf((char) 160);
+	private static final String INPUT = "  \tlibor\t \n\r" + NBSP + NBSP + "ondrusek   \t\t\n\n\r  ";
+	private static final String EXPECTED = "libor   ondrusek";
+
+	public void timeApacheNormalize(int reps) {
+		for (int i = 0; i < reps; i++) {
+			String normalized = StringUtils.normalizeSpace(INPUT);
+			assertEquals(EXPECTED, normalized);
+		}
+	}
+
+	public void timeNewNormalize(int reps) {
+		for (int i = 0; i < reps; i++) {
+			String normalized = tmsdp.util.StringUtils.normalizeAndReplaceWhitespaces(INPUT);
+			assertEquals(EXPECTED, normalized);
+		}
+	}
+
+	@Test
+	public void benchmark() throws Exception {
+		Runner.main(NormalizeSpaces.class, new String[0]);
+	}
+
+	@Test
+	public void testApacheNormalize() throws Exception {
+		assertEquals(EXPECTED, StringUtils.normalizeSpace(INPUT));
+	}
+
+	@Test
+	public void testNewNormalize() throws Exception {
+		assertEquals(EXPECTED, tmsdp.util.StringUtils.normalizeAndReplaceWhitespaces(INPUT));
+	}
+}
